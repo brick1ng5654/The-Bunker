44cfa958ad277e300a0b774d1edad138bfdb7842
brick1ng5654 <Bricking5654Help@yandex.ru>
2024-11-13 12:04:30 +0300

    rework of menu
    a little bit changes in bot menu, create admin user

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ import logging
 logger = logging.getLogger("game_logger")
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()  # Для вывода логов в консоль
-handler = logging.FileHandler('game.log', encoding='utf-8')
+# handler = logging.FileHandler('game.log', encoding='utf-8')
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import Application, CommandHandler, CallbackContext, Updater
+from telegram.ext import Application, CommandHandler, CallbackContext, Updater, filters, MessageHandler, ContextTypes
 import random
 from logger import logger
 import os
@@ -13,101 +13,128 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 # Запускаем приложение
 app = Application.builder().token(TOKEN).build()
 
-# Списки для хранения информации о количестве игроков, игроках, их характеристиках и информация о сессии
-player_number = 1
-players = {}
-game_active = False
-admin_id = 0
+# Глобальные переменные
+player_number = 1 # Количество игроков в сессии
+players = {} # Словарь игроков
+session_active = False # Активна ли игра
+admin = 0 # Админ сессии
 
 async def start(update: Update, context: CallbackContext) -> None:
     user = update.message.from_user
     logger.info(f"{user.username} начал общение с ботом")
-    welcome_message = f"Привет, {user.first_name}! Я бот, предназначенный для игры в Бункер. По кнопкам ниже ты можешь осуществлять навигацию"
-    # Создание кнопок
+
+    welcome_message = f"Привет, {user.first_name}! Я бот, предназначенный для игры в Бункер. По кнопкам ниже ты можешь осуществлять навигацию."
+    await update.message.reply_text(welcome_message)
+    await menu(update, context)
+
+# Обработчик текстовых сообщений, реагирующий на команды без "/"
+
+async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text  # Получаем текст сообщения
+    user = update.message.from_user
+    text = text.lower()
+    
+    if text == "создать сессию":
+        await create_session(update, context)
+    elif text == "присоединиться к сессии":
+        await update.message.reply_text("подключение к сессии")
+    elif text == "участники":
+        await members(update, context)
+    elif text == "отключиться":
+        await disconnect(update, context)
+    elif text == "главное меню":
+        await menu(update, context)
+    else:
+        logger.info(f"{user.first_name}: {text}")
+        await update.message.reply_text("Неизвестная команда. Пожалуйста, выберите действие на клавиатуре.")
+
+async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global session_active
+    if session_active:
+        update.message.reply_text("Сначала покиньте активную сессию")
+        return
     keyboard = [
-        ["Создать комнату", "Присоединиться к комнате"]
+        ["Создать сессию", "Присоединиться к сессии"]
     ]
+
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True,one_time_keyboard=True)
-    await update.message.reply_text(welcome_message, reply_markup=reply_markup)
+    await update.message.reply_text("Главное меню", reply_markup=reply_markup)
 
-# Обработчик команды "Создать комнату"
-async def create_room(update: Update, context: CallbackContext) -> None:
-    global game_active, player_number, admin_id
+# Функция для обработки "Создать сессию"
+async def create_session(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global admin, session_active, player_number
     user = update.message.from_user
-    # Проверка, активна ли уже игра
-    if game_active:
-        await update.message.reply_text("Игра уже запущена. Присоединяйтесь!")
-        logger.info(f"Пользователь {user.first_name} попытался безуспешно создать игру")
+    # Проверка не создана ли сессия ранеее
+    if (session_active):
+        if user.id == admin.id:
+            await update.message.reply_text("Вы уже создали сессию!")
+            logger.info(f"Пользователь {user.username} попытался заново создать сессию")
+            return
+        await update.message.reply_text("Сессия уже начата! Присоединяйся!")
+        logger.info(f"Пользователь {user.username} попытался создать сессию")
         return
-    
-    # Запуск игры и добавление первого игрока
-    game_active = True
-    admin_id = update.message.from_user
-    logger.info(f"Пользователь {user.first_name} создал комнату для игры")
-    player_id = user.id
-    players[player_id] = Player(user.first_name, player_number, player_id)
-    logger.info(players[player_id])
-    
-    keyboard = [ ["Начать игру"] ]
-    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
 
-    await update.message.reply_text(f"Игра скоро начнётся! Чтобы начать игру с текущим количеством игроков нажмите кнопку ниже", reply_markup=reply_markup)
-    await update.message.reply_text("Другие игроки могут присоединиться к игре с помощью команды /join_game.")
-    player_number+=1
-    
-# Обработчик команды /join_game
-async def join_game(update: Update, context: CallbackContext) -> None:
-    global game_active, player_number
+    # Создание сессии
+    session_active = True
+    admin = user
+    logger.info(f"Пользователь {user.username} создал сессию и является её админом")
+    players[user.id] = [user.id, user.first_name, user.username]
+
+    keyboard = [
+        ["Начать игру", "Участники"],
+        ["Отключиться"]
+    ]
+
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=False)
+    await update.message.reply_text("Вы начали сессию! Другие участники могут к ней присоединиться.", reply_markup=reply_markup)
+
+async def members(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global session_active
+
     user = update.message.from_user
-    
-    if not game_active:
-        await update.message.reply_text("Игра ещё не началась. Используйте /start_game для запуска.")
-        logger.info(f"Пользователь {user.first_name} попытался присоединится к несуществующей игре")
-        return
-    
-    player_id = user.id
-    
-    if player_id in players:
-        await update.message.reply_text("Вы уже зарегистрированы в игре!")
-        logger.info(f"Пользователь {user.first_name} попытался присоединится к уже присоединенной игре")
-        return
-    
-    players[player_id] = Player(user.first_name ,player_number, player_id)
-    player_number+=1
-    await update.message.reply_text(f"{user.first_name} присоединился к игре!")
-    logger.info(f"Пользователь {user.first_name} присоединился к игре")
 
-async def my_info(update: Update, context: CallbackContext) -> None:
-    global game_active
+    if session_active:
+        if(user.id in players.keys()):
+            await update.message.reply_text("Список участников сессии:")
+            for player_info in players.values():
+                await update.message.reply_text(f"{player_info[1]}")
+            return
+        else:
+            await update.message.reply_text("Вы не присоединены к сессии")
+    await update.message.reply_text("Сессия ещё не начата. Быстрее создавай и приглашай друзей!")
+
+async def disconnect(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global session_active
+
     user = update.message.from_user
-    
-    if not game_active:
-        await update.message.reply_text("Игра ещё не началась. Используйте /start_game для запуска.")
-        logger.info(f"Пользователь {user.first_name} попытался присоединится к несуществующей игре")
-        return
+    if session_active:
+        if user.id in players.keys():
+            if (admin.id == user.id):
+                logger.info(f"{admin.username} завершил сессию:")
+                session_active = False
+                for user_id, user_info in list(players.items()):
+                    logger.info(f"{user_info[2]} покинул сессию")
+                    players.pop(user_id, None)
+                await update.message.reply_text("Вы завершили сессию")
+                await menu(update, context) 
+                return
+            else:
+                logger.info(f"{user.username} покинул сессию")
+                players.pop(user.id, None)
+                await update.message.reply_text("Вы покинули сессию")
+                await menu(update, context) 
+                return
+        else:
+            await update.message.reply_text("Вы не подключены к сессии")
+            await menu(update, context) 
+            return
+    await update.message.reply_text("Вы не подключены к сессии")   
 
-    await update.message.reply_text(f"{players[user.id].return_info()}")
-    logger.info(f"Пользователь {user.first_name} написал /my_info")
-
-# async def my_info(update: Update, context: CallbackContext) -> None:
-#     global game_active
-#     user = update.message.from_user
-    
-#     if not game_active:
-#         await update.message.reply_text("Игра ещё не началась. Используйте /start_game для запуска.")
-#         logger.info(f"Пользователь {user.first_name} попытался присоединится к несуществующей игре")
-#         return
-
-#     for player in players.values():
-#         await update.message.reply_text(f"{player.return_info()}")
-#         logger.info(f"Пользователь {user.first_name} написал /my_info")
-
-
-# Регистрация команд
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("Создать комнату", create_room))
-app.add_handler(CommandHandler("join_game", join_game))
-app.add_handler(CommandHandler("my_info", my_info))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
+app.add_handler(MessageHandler(filters.TEXT & filters.Regex("(?i)^Создать сессию$"), create_session))
+app.add_handler(MessageHandler(filters.TEXT & filters.Regex("(?i)^Участники$"), members))
+app.add_handler(MessageHandler(filters.TEXT & filters.Regex("(?i)^Отключиться$"), disconnect))
 
 # Запуск бота
 if __name__ == "__main__":
